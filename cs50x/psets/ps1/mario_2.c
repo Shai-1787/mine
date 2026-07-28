e0d119c0f80a3f8583ac44f6709e6c2ccd653e0a
Shai Rachel <safklas087@gmail.com>
2026-07-28 20:07:03 +0800

    complete 2 marios

---
+++ cs50x/psets/ps1/mario_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Height;
+    do
+    {
+        printf("Height: ");
+        scanf("%d", &Height);
+    }
+    while (Height < 1 || Height > 8);
+
+    for (int i = 0; i < Height; i++)
+    {
+        for (int j = 0; j < Height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+
+        for (int l = 0; l < i + 1; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}   
