e4fbb280cca59ce2979dbb25aa859096f724099c
Shai Rachel <safklas087@gmail.com>
2026-07-30 22:10:16 +0800

    complete readability.c

---
+++ cs50x/psets/ps2/readability.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char text[1000];
+    printf("Text: ");
+    fgets(text, sizeof(text), stdin);
+
+    int letters = 0;
+    int words = 1; // Start with 1 to account for the first word
+    int sentences = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    float L = (float)letters / words * 100;
+    float S = (float)sentences / words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %.0f\n", index);
+    } 
+}
