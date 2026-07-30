0755a3054951e6deba707af362f72685e17e6085
Shai Rachel <safklas087@gmail.com>
2026-07-30 15:12:27 +0800

    complete scrabble.c

--- cs50x/psets/ps2/scrabble.c
+++ cs50x/psets/ps2/scrabble.c
@@ -5,62 +5,35 @@ int main(void)
     int scores[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
     char word[32];
+    int player_score[2] = {0, 0};
 
-    do
-    {
-        printf("Player 1: ");
+    for (int player = 0; player < 2; player++)
+    {      
+        printf("Player %d: ", player + 1);
         if (scanf("%31s", word) != 1)
         {
-            return 1; // Exit if input is not a valid string
+            return 1;
         }
-    }
-    while (word[0] == '\0'); // Repeat until a non-empty word is entered
-
-    // Calculate score for Player 1
-    int score_1 = 0;
-    for (int i = 0; word[i] != '\0'; i++)
-    {
-        char letter = word[i];
-        if (letter >= 'A' && letter <= 'Z')
-        {
-            score_1 += scores[letter - 'A'];
-        }
-        else if (letter >= 'a' && letter <= 'z')
-        {
-            score_1 += scores[letter - 'a'];
-        }
-    }
-
-    do
-    {
-        printf("Player 2: ");
-        if (scanf("%31s", word) != 1)
-        {
-            return 1; // Exit if input is not a valid string
-        }
-    }
-    while (word[0] == '\0'); // Repeat until a non-empty word is entered
     
-    int score_2 = 0;
-    for (int i = 0; word[i] != '\0'; i++)
-    {
-        char letter = word[i];
-        if (letter >= 'A' && letter <= 'Z')
+        for (int i = 0; word[i] != '\0'; i++)
         {
-            score_2 += scores[letter - 'A'];
-        }
-        else if (letter >= 'a' && letter <= 'z')
-        {
-            score_2 += scores[letter - 'a'];
+            char c = word[i];
+            if (c >= 'A' && c <= 'Z')
+            {
+                player_score[player] += scores[c - 'A'];
+            }
+            else if (c >= 'a' && c <= 'z')
+            {
+                player_score[player] += scores[c - 'a'];
+            }
         }
     }
     
-    // Determine the winner
-    if (score_1 > score_2)
+    if (player_score[0] > player_score[1])
     {
         printf("Player 1 wins!\n");
     }
-    else if (score_2 > score_1)
+    else if (player_score[1] > player_score[0])
     {
         printf("Player 2 wins!\n");
     }
